9ec193491ac1d1376434a70b43caacc3999b5d8e
MikeDesktop <micheledong2803@gmail.com>
2024-04-16 14:30:02 +0200

    bonus

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let nome = prompt("Inserisci il tuo nome: ");
 let cognome = prompt("Inserisci il tuo cognome: ");
 let colore = prompt("Inserisci il tuo colore preferito: ");
-document.writeln(nome+cognome+colore+"#24");
+document.writeln("La tua password è: &lt;"+nome+cognome+colore+"#24&gt;");
+alert("La tua password è: <"+nome+cognome+colore+"#24>");
