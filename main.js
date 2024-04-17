7e16e88fa28c25a2cca4d1d68defa97116a9a391
michele laptop <micheledong2803@gmail.com>
2024-04-17 11:50:50 +0200

    console.log

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-let nome = prompt("Inserisci il tuo nome: ");
-let cognome = prompt("Inserisci il tuo cognome: ");
-let colore = prompt("Inserisci il tuo colore preferito: ");
-document.writeln("La tua password è: &lt;"+nome+cognome+colore+"#24&gt;");
-alert("La tua password è: <"+nome+cognome+colore+"#24>");
+let nome = prompt("Inserisci il tuo nome: "),cognome = prompt("Inserisci il tuo cognome: "),colore = prompt("Inserisci il tuo colore preferito: "); 
+document.writeln("La tua password è: &lt;"+nome+cognome+colore+"#24&gt;"); //lavora sull'html
+console.log("La tua password è: <"+nome+cognome+colore+"#24>");
